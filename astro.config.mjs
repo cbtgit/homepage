bf273b17b211046c8a9f2d0fda8453997101ea35
Christian Bloch Thomsen <cblochth@gmail.com>
2026-08-12 22:06:49 +0200

    Prepare site for indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://frontend-freelance.dk',
+  integrations: [sitemap()],
+});
